0732f796b7ff4d8735270f41eafe8e166ccd6542
mihaylovea <mihailovevgenya@gmail.com>
2025-06-06 10:05:58 +0300

    cors and spectacular final

--- config/urls.py
+++ config/urls.py
@@ -18,22 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny, ],
-)
 
 urlpatterns = [
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
